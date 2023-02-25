cdea880e488e316231da3236efd4a8c5d477bdfb
unknown <samir-grashin@mail.ru>
2023-02-25 23:09:24 +0300

    Example5

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Как вас зовут?");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Приевет,Маша, мы очень по тебе скучали!!!!"); 
+}
+else 
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
