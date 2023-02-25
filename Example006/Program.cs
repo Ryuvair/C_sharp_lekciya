4a5e1fae0ede088666fdbdcdef4bd18d71ffe451
unknown <samir-grashin@mail.ru>
2023-02-25 23:16:22 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 10;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 9; 
+
+int max = a; 
+ 
+if (b > max) max = b ;
+if (c > max) max = c ;
+if (d > max) max = d ;
+if (e > max) max = e ;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
+
+
