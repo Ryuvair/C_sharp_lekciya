8e1199e7001cc661111ece0bacf03dde0cafac2f
unknown <samir-grashin@mail.ru>
2023-02-28 19:51:12 +0300

    2.Example004

---
+++ C#_lekciya2/2.Example004/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray (int[] collection) // VOID - метод ничего не возвращаетнужен войд
+{
+int length =  collection.Length;
+int index = 0;
+while (index < length)
+{
+    collection[index] = new Random().Next(1, 10);
+    index++;
+
+}
+
+}
+void PrintArray(int[] col) 
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+
+int [] array =new int  [10];
+
+FillArray(array);
+PrintArray(array);
