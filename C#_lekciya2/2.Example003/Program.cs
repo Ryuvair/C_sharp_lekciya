fd73573b495e610ec60dbb54cd30aa0629d84fba
unknown <samir-grashin@mail.ru>
2023-02-28 19:33:10 +0300

    2.Example003

---
+++ C#_lekciya2/2.Example003/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {7,0,7,4,2,8,9,0,72};
+
+int n = array.Length; // Определяет колличество элеменнтов масива (длину масива)
+int  find = 0;
+
+int index = 0;
+
+while (index< n)
+ {
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+
+    }
+    index++; // index = index + 1
+
+} 
